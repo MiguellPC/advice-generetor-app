845dfbda81f4f19fa2c78c971d10eb369da30a03
Miguel Pereira <miguelp326@gmail.com>
2023-04-21 00:43:15 -0300

    add: loading animation

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const url = 'https://api.adviceslip.com/advice';
 
+const container = document.querySelector('.container');
 const adviceID = document.querySelector('#adviceID');
 const adviceText = document.querySelector('#adviceText');
 const adviceBtn = document.querySelector('#adviceBtn');
+
+const displayLoading = () => {
+  const div = document.createElement('div');
+  div.classList.add('loading-container');
+  div.innerHTML = `
+    <div class="gooey">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  `;
+
+  container.removeChild(adviceID);
+  container.removeChild(adviceText);
+
+  container.prepend(div);
+}
+
+const removeLoading = () => {
+  const loading = document.querySelector('.loading-container');
+  if (loading) {
+    setTimeout(() => {
+      loading.remove();
+      container.prepend(adviceText);
+      container.prepend(adviceID);
+    }, 2000);
+  }
+}
 
 const fetchAdvice = () => {
   if (adviceID.innerHTML !== '' && adviceText.innerHTML !== '') {
@@ -10,11 +42,14 @@ const fetchAdvice = () => {
     adviceText.innerHTML = '';
   }
 
+  displayLoading();
+
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      adviceID.innerHTML = 'ADVICE #' + data.slip.id;
-      adviceText.innerHTML = '"' + data.slip.advice + '"';
+      removeLoading();
+      adviceID.innerHTML = `ADVICE # ${data.slip.id}`;
+      adviceText.innerHTML = `"${data.slip.advice}"`;
     })
     .catch(err => console.log(err));
 }
